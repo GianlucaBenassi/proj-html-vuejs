ad5e11ce223c1399de511c1a41a4b5e8bb5f2002
Gianluca Benassi <benox1199@gmail.com>
2022-01-11 20:41:24 +0100

    Courses array

--- src/components/sections/Courses.vue
+++ src/components/sections/Courses.vue
@@ -22,6 +22,54 @@ export default {
     name: 'Courses',
     components: {
         CourseCard
+    },
+    data() {
+        return {
+            Courses: [
+                {
+                    title: 'Learning to Write as a Professional Author',
+                    price: 40.00,
+                    img: require('../../assets/img/course-02-480x298.jpg'),
+                    lessons: 20,
+                    students: 50
+                },
+                {
+                    title: 'Customer-centric Info-Tech Strategies',
+                    price: 'Free',
+                    img: require('../../assets/img/stock-full-hd-03-480x298.jpg'),
+                    lessons: 24,
+                    students: 769
+                },
+                {
+                    title: 'Open Programming Courses for Everyone: Python',
+                    price: 19.00,
+                    img: require('../../assets/img/stock-full-hd-04-480x298.jpg'),
+                    lessons: 17,
+                    students: 62
+                },
+                {
+                    title: 'Academic Listening and Note-taking',
+                    price: 26.00,
+                    img: require('../../assets/img/stock-full-hd-06-480x298.jpg'),
+                    lessons: 14,
+                    students: 67
+                },
+                {
+                    title: 'Master jQuery in a Short Period of Time',
+                    price: 39.00,
+                    img: require('../../assets/img/course-featured-image-01-480x298.jpg'),
+                    lessons: 6,
+                    students: 51
+                },
+                {
+                    title: 'Introduction to Javascript for Beginners',
+                    price: 59.00,
+                    img: require('../../assets/img/stock-full-hd-05-480x298.jpg'),
+                    lessons: 14,
+                    students: 76
+                }
+            ]
+        }
     }
 }
 </script>

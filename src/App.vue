ebbe1e2de2085a2efcc871be400b70fc3fd38238
Gianluca Benassi <benox1199@gmail.com>
2022-01-11 11:51:03 +0100

    navbar array links

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,89 @@ export default {
     Header,
     Main,
     Footer
+  },
+  data() {
+    return {
+      navText: [
+        {
+          title: 'Home',
+          links: [
+            'MaxCoach Education',
+            'Course Portal',
+            'Distant Learning',
+            'Multimedia Pedagogy',
+            'Modern Schooling',
+            'Remote Training',
+            'Health Coaching',
+            'Gym Coaching',
+            'Business',
+            'Artist',
+            'Kitchen Coach',
+            'Motivation',
+            'Dancing',
+            'Guitar',
+            'Yoga',
+            'Photography',
+            'Personal Finance'
+          ]
+        },
+        {
+          title: 'Pages',
+          Links: [
+            'Start Here',
+            'Success Story',
+            'About me',
+            'About us 01',
+            'About us 02',
+            'About us 03',
+            'Contact me',
+            'Contact us',
+            'Purchase Guide',
+            'Privacy Policy',
+            'Terms of Service'
+          ]
+        },
+        {
+          title: 'Courses',
+          Links: [
+            'Courses Grid 01',
+            'Courses Grid 02',
+            'Courses Grid 03',
+            'Membership Levels',
+            'Become a Teacher',
+            'Profile',
+            'Checkout',
+            'Single Layout'
+          ]
+        },
+        {
+          title: 'Features',
+          Links: [
+            'Events',
+            'Zoom Meetings'
+          ]
+        },
+        {
+          title: 'Blog',
+          Links: [
+            'Blog Grid',
+            'Blog Masonry',
+            'Blog Classic',
+            'Blog List'
+          ]
+        },
+        {
+          title: 'Shop',
+          Links: [
+            'Shop Left Sidebar',
+            'Shop Right Sidebar',
+            'Cart',
+            'Wishlist',
+            'Single Product'
+          ]
+        }
+      ]
+    }
   }
 }
 </script>

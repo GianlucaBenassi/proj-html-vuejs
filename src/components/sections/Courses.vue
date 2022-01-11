6e94792e11d743891b8108b28960cff3166ca579
Gianluca Benassi <benox1199@gmail.com>
2022-01-11 17:49:00 +0100

    CourseCard component creation

--- src/components/sections/Courses.vue
+++ src/components/sections/Courses.vue
@@ -1,26 +1,32 @@
 <template>
     <section class="section-bg-grey">
-        <div class="container">
+        <div class="container py-5">
             
             <h3 class="sub-title text-center">Stimulated to learn?</h3>
-            <h2 class="medium-title text-center">Featured Online Courses</h2>
+            <h2 class="mt-2 medium-title text-center">Featured Online Courses</h2>
+
+            <div class="row">
+                <div class="col-6">
+                    <CourseCard/>
+                </div>
+            </div>
 
         </div>
     </section>
 </template>
 
 <script>
+import CourseCard from '../commons/CourseCard.vue';
+
 export default {
-    name: 'Courses'
+    name: 'Courses',
+    components: {
+        CourseCard
+    }
 }
 </script>
 
 <style lang='scss' scoped>
-
-.container {
-    padding: 50px 0;
-}
-
 
 
 </style>

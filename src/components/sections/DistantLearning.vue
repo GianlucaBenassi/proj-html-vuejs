60474423ee2bf37cb7f6fbf439ab7daacfa9892c
Gianluca Benassi <benox1199@gmail.com>
2022-01-11 15:54:15 +0100

    DistantLearning title style

--- src/components/sections/DistantLearning.vue
+++ src/components/sections/DistantLearning.vue
@@ -1,6 +1,34 @@
 <template>
     <section>
+        <div class="container">
 
+            <!-- title row -->
+            <div class="row">
+
+                <!-- title -->
+                <div class="col title">
+                    <h1 class="big-title">Distant learning for futher expansion</h1>
+                    <p class="text-color-light">Learning is a life-long journey that in fact we find the terminate stop. Stop searching, enjoy the process.</p>
+                    <button><i class="fas fa-download"></i> Download free guide</button>
+                    <a href="#" class="text-color-light">
+                        Have questions? 
+                        <span class="text-green">Get Free Sample <i class="fas fa-long-arrow-alt-right"></i></span>
+                        </a>
+                </div>
+
+                <!-- image -->
+                <div class="col">
+
+                </div>
+
+            </div>
+
+            <!-- logos row -->
+            <div class="row">
+
+            </div>
+
+        </div>
     </section>
 </template>
 
@@ -11,5 +39,28 @@ export default {
 </script>
 
 <style lang='scss' scoped>
+
+.container {
+    padding: 50px 0;
+}
+
+.title {
+
+    p {
+        margin-top: 10px;
+    }
+
+    button {
+        margin: 40px 0 20px 0;
+    }
+
+    a {
+        display: block;
+
+        span {
+            font-weight: bold;
+        }
+    }
+}
 
 </style>

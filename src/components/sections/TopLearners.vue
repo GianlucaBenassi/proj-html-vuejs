d46fa5ab1e858619c7d814749e06df3c93f97b71
Gianluca Benassi <benox1199@gmail.com>
2022-01-12 10:46:33 +0100

    TopLearners title

--- src/components/sections/TopLearners.vue
+++ src/components/sections/TopLearners.vue
@@ -2,7 +2,15 @@
     <section>
 
         <!-- wave -->
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#f6f3f5" fill-opacity="1" d="M0,224L120,229.3C240,235,480,245,720,208C960,171,1200,85,1320,42.7L1440,0L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"></path></svg>
+        <svg class="d-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#f6f3f5" fill-opacity="1" d="M0,224L120,229.3C240,235,480,245,720,208C960,171,1200,85,1320,42.7L1440,0L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"></path></svg>
+
+        <div class="container-fluid section-bg-grey pb-5">
+            
+            <!-- title -->
+            <h3 class="sub-title text-center">Great words about maxcoach</h3>
+            <h2 class="mt-2 mb-5 medium-title text-center">Our <span class="text-green">top learners'</span> verbatim</h2>
+
+        </div>
 
     </section>
 </template>
@@ -14,5 +22,9 @@ export default {
 </script>
 
 <style lang='scss' scoped>
+
+h2 span {
+    font-weight: normal;
+}
 
 </style>

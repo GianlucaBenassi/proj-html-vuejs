a69ad351ce22a2f3597029b265481519d63cdffc
Gianluca Benassi <benox1199@gmail.com>
2022-01-12 14:25:06 +0100

    Data component layout

--- src/components/sections/Data.vue
+++ src/components/sections/Data.vue
@@ -1,6 +1,34 @@
 <template>
     <section>
-        
+        <div class="container">
+            <div class="row">
+
+                <div class="col text-center border-left-grey border-right-grey p-5">
+                    <h3>Successfully trained</h3>
+                    <span class="text-green big-number">1790</span>
+                    <h3 class="sub-title">enrolled learners</h3>
+                </div>
+
+                <div class="col text-center border-right-grey p-5">
+                    <h3>Prodly Received</h3>
+                    <span class="text-green big-number">19</span>
+                    <h3 class="sub-title">countrywide awards</h3>
+                </div>
+
+                <div class="col text-center border-right-grey p-5">
+                    <h3>Firmly Established</h3>
+                    <span class="text-green big-number">24</span>
+                    <h3 class="sub-title">local branches</h3>
+                </div>
+
+                <div class="col text-center border-right-grey p-5">
+                    <h3>Getting Featured on</h3>
+                    <span class="text-green big-number">1090</span>
+                    <h3 class="sub-title">blog posts</h3>
+                </div>
+
+            </div>
+        </div>
     </section>
 </template>
 
@@ -11,5 +39,10 @@ export default {
 </script>
 
 <style lang='scss' scoped>
+
+.big-number {
+    font-size: 3rem;
+    font-weight: bold;
+}
 
 </style>

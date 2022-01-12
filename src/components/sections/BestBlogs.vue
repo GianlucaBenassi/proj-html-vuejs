8d8d475aa046615e23d94ab1989b0c6fbc0009c5
Gianluca Benassi <benox1199@gmail.com>
2022-01-12 14:29:11 +0100

    BestBlogs component creation

---
+++ src/components/sections/BestBlogs.vue
@@ -0,0 +1,27 @@
+<template>
+    <section class="section-bg-grey">
+        <div class="container py-5">
+            <div class="row">
+
+                <div class="col-4">
+
+                </div>
+
+                <div class="col-8">
+
+                </div>
+
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'BestBlogs'
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>

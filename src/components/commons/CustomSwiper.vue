d7f8541b97d3569945467118bbfdd932c9103bb4
Gianluca Benassi <benox1199@gmail.com>
2022-01-13 15:00:27 +0100

    CustomSwiper responsive

--- src/components/commons/CustomSwiper.vue
+++ src/components/commons/CustomSwiper.vue
@@ -69,8 +69,20 @@ export default {
     data() {
         return {
             swiperOptions: {
-                slidesPerView: 3,
-                spaceBetween: 30,
+                breakpoints: {
+                    0: {
+                        slidesPerView: 1,
+                        spaceBetween: 20
+                    },
+                    768: {
+                        slidesPerView: 2,
+                        spaceBetween: 30
+                    },
+                    992: {
+                        slidesPerView: 3,
+                        spaceBetween: 40
+                    }
+                },
                 loop: true,
                 centeredSlides: true,
                 speed: 800,

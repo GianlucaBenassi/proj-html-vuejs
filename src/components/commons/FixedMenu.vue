70faa016f1ddac688ced8887939f521f96574219
Gianluca Benassi <benox1199@gmail.com>
2022-01-13 10:24:18 +0100

    FixedMenu baloon

--- src/components/commons/FixedMenu.vue
+++ src/components/commons/FixedMenu.vue
@@ -5,24 +5,40 @@
             <li>
                 <a href="#" class="d-flex justify-content-center align-items-center text-color">
                     <i class="fas fa-ruler-combined"></i>
+                    <div class="baloon px-3 py-2">
+                        <span>Select Demo</span>
+                        <div class="corner"></div>
+                    </div>
                 </a>
             </li>
 
             <li>
                 <a href="#" class="d-flex justify-content-center align-items-center text-color">
                     <i class="far fa-dot-circle"></i>
+                    <div class="baloon px-3 py-2">
+                        <span>Support Channel</span>
+                        <div class="corner"></div>
+                    </div>
                 </a>
             </li>
 
             <li>
                 <a href="#" class="d-flex justify-content-center align-items-center text-color">
                     <i class="fas fa-book"></i>
+                    <div class="baloon px-3 py-2">
+                        <span>Documentation</span>
+                        <div class="corner"></div>
+                    </div>
                 </a>
             </li>
 
             <li>
                 <a href="#" class="d-flex justify-content-center align-items-center text-color">
                     <i class="fas fa-shopping-cart"></i>
+                    <div class="baloon px-3 py-2">
+                        <span>Purchase Maxcoach</span>
+                        <div class="corner"></div>
+                    </div>
                 </a>
             </li>
 
@@ -49,13 +65,47 @@ export default {
     z-index: 9990;
 
     li {
-       width: 40px;
-       height: 40px;
+        width: 40px;
+        height: 40px;
 
-       a {
-           width: 100%;
-           height: 100%;
-       }
+        a {
+            width: 100%;
+            height: 100%;
+            position: relative;
+
+            .baloon {
+                display: none;
+                background-color: #000;
+                box-shadow: 0px 0px 10px 5px rgba(0,0,0,.2);
+                border-radius: 5px;
+                position: absolute;
+                top: 50%;
+                right: 140%;
+                transform: translate(0, -50%);
+
+                span {
+                    color: #fff;
+                    font-size: .9rem;
+                    font-weight: bold;
+                    white-space: nowrap;
+                }
+
+                .corner {
+                    width: 15px;
+                    height: 15px;
+                    background-color: black;
+                    box-shadow: 0px 0px 10px 5px rgba(0,0,0,.2);
+                    position: absolute;
+                    top: 50%;
+                    right: -5px;
+                    transform: translate(0, -50%) rotate(45deg);
+                }
+            }
+
+            &:hover .baloon {
+                display: block;
+            }
+        }
     }
     
 }
